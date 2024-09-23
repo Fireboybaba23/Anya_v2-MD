8971b667b92f294a5b071d78e0a40cc8ed69406d
Fireboybaba23 <fireboyyoung6@gmail.com>
2024-09-23 10:03:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "Cr7 supreme",
+    ownernumber: process.env.Owner_Number || "2348139598985",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "cHFvN_Queen-Anya_WN_Queen-Anya_ob2E=",
 
     /**
      * thumbnails and medias
